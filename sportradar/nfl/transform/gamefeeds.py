b35e355c616b636b0a84d0a9b52894bf06e7d615
Gaurav Gurjar <ggurjar333@gmail.com>
2024-03-01 11:27:48 +0000

    add _id to unwanted_keys and adds flatten_dict function

--- sportradar/nfl/transform/gamefeeds.py
+++ sportradar/nfl/transform/gamefeeds.py
@@ -14,7 +14,7 @@ class GameFeedsTransformer:
         transform_game_statistics: Transforms the game statistics data.
     """
 
-    UNWANTED_KEYS = ["_comment"]
+    UNWANTED_KEYS = ["_comment", "_id"]
 
     def __init__(self, data: dict):
         self.data = data
@@ -24,6 +24,18 @@ class GameFeedsTransformer:
         for key in self.UNWANTED_KEYS:
             if key in self.data:
                 self.data.pop(key)
+
+    def flatten_dict(self, parent_key='', sep='.'):
+        flattened_dict = {}
+        for key, value in self.data.items():
+            new_key = f"{parent_key}{sep}{key}" if parent_key else key
+
+            if isinstance(value, dict):
+                flattened_dict.update(flatten_dict(value, new_key, sep=sep))
+            else:
+                flattened_dict[new_key] = value
+
+        return flattened_dict
 
     def transform_boxscore(self):
         return self.data
